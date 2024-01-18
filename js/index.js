af9e54822544625d61cf5426cdfbf2fdcbf743d5
afifudin23 <afifu5882@gmail.com>
2024-01-19 02:54:33 +0700

    add services section

--- js/index.js
+++ js/index.js
@@ -5,3 +5,39 @@ const scrollHeader = () =>{
     this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+// =========== SERVICE MODAL ===========
+const modalViews = document.querySelectorAll(".services_modal");
+const modalBtns = document.querySelectorAll(".services_button");
+const modalClose = document.querySelectorAll(".services_modal-close");
+
+const modal = (modalClick) => {
+    modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener("click", () => {
+        console.log(modalClose);
+        modal(i);
+    })
+});
+console.log(modalClose);
+modalClose.forEach((mc) => {
+    mc.addEventListener("click", () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove("active-modal");
+        })
+    })
+});
+
+// =========== MIXITUP FILTER POIRTFOLIO ===========
+
+/* Link Active Work */
+
+// ============== SWIPER TESTIMONIAL ==============
+
+// ==============   SCROLL SELSI HFFL ==============
+
+// ============== SWIPER TESTIMONIAL ==============
+
+// ============== LIGHT DARK THEME ==============
