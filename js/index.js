3454e1b8f19081efa96a498780477139d0093600
afifudin23 <afifu5882@gmail.com>
2024-02-14 09:44:34 +0700

    finish project

--- js/index.js
+++ js/index.js
@@ -122,5 +122,14 @@ themeButton.addEventListener('click', () => {
 })
 
 // ============== SCROLL REVEAL ANIMATION ==============
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2000,
+    // delay: 400,
+    // reset: true
+});
 
-
+sr.reveal(".home_data");
+sr.reveal(".home_handle", {delay: 700});
+sr.reveal(".home_social, .home_scroll", {delay: 900, origin: "bottom"});
